21e00eec6aefdc86cda803e2bff8797c271537e2
eruiz <eruiz@absis.es>
2021-11-23 19:44:40 +0100

    Completed module

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,10 +1,12 @@
 const connection = new WebSocket("ws://localhost:3000");
 const logWindow = document.querySelector("#log-window");
+const filePath = document.getElementById('logFilePath').value;
 
 connection.onopen = function() {
-    connection.send("Hello from the client!");
+    connection.send(filePath  ? filePath  : "Hello from the client!");
 }
 
 connection.onmessage = function(event) {
-    logWindow.innerHTML = event.data;
+    const logs = event.data.split("\n").join("<hr>");
+    logWindow.innerHTML = logs;
 }
