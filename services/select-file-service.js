7f0f409dc2b086384e5e7bef03b16860120f4a87
eruiz <eruiz@absis.es>
2021-11-23 20:22:48 +0100

    Completed module

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,11 +1,41 @@
+const fs = require("fs");
+const path = require("path");
+
+const dir = process.cwd();
+
 function getDirectoryContents(files, currentDir, query) {
+    const data = [];
+    files.forEach(function(file) {
+        if (isDirectory(currentDir, file)) {
+            data.push({name: file, isDirectory: true, path: path.join(query, file)})
+        } else {
+            data.push({name: file, isDirectory: false, path: path.join(query, file), currentDir: currentDir})
+        }
+    })
+    return data;
 }
 
 function isDirectory(currentDir, file) {
+    const fileInfo = fs.statSync(path.join(currentDir, file));
+    return fileInfo.isDirectory();
 }
 
 function readDir(currentDir, res, query) {
+    fs.readdir(currentDir, function(err, files) {
+        var directoryContents = [];
+        if (!err) {
+            directoryContents = getDirectoryContents(files, currentDir, query);
+        }
+        res.json(directoryContents);
+    })
 }
 
 exports.get = (req, res) => {
+    var currentDir = dir;
+    const query = req.query.path ? req.query.path : "";
+    if (query) {
+        currentDir = path.join(currentDir, query);
+    }
+
+    readDir(currentDir, res, query)
 };
